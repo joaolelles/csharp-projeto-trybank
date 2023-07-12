d6de87df41b8bc931831b74db653b58d99a70b76
João <joaolelles7@gmail.com>
2023-07-12 17:22:07 -0300

    Requisito 7, Feat: Construindo a funcionalidade de transferir dinheiro entre contas

--- src/trybank/Trybank.cs
+++ src/trybank/Trybank.cs
@@ -117,7 +117,23 @@ public class Trybank
     // 7. Construa a funcionalidade de transferir dinheiro entre contas
     public void Transfer(int destinationNumber, int destinationAgency, int value)
     {
-        throw new NotImplementedException();
+        if (!Logged)
+        {
+            throw new AccessViolationException("Usuário não está logado");
+        }
+        if (value > Bank[loggedUser, 3])
+        {
+            throw new InvalidOperationException("Saldo insuficiente");
+        }
+
+        for (int i = 0; i < registeredAccounts; i++)
+        {
+            if (Bank[i, 0] == destinationNumber && Bank[i, 1] == destinationAgency)
+            {
+                Bank[loggedUser, 3] -= value;
+                Bank[i, 3] += value;
+            }
+        }
     }
 
 
